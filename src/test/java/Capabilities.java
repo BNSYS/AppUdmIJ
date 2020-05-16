7ccbd838b9c5f03c442d3bc96f02228e654a0da2
Andrey Sokolovskiy <andrey.sokolovskiy@ttms.pl>
2020-05-16 20:26:32 +0200

    Gestures part 2, Capabilities

--- src/test/java/Capabilities.java
+++ src/test/java/Capabilities.java
@@ -1,6 +1,7 @@
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
@@ -14,6 +15,7 @@ public class Capabilities {
         URL servUrl = new URL("http://127.0.0.1:4723/wd/hub");
 
         DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
         cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel2API28");
         cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
         cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
@@ -21,5 +23,69 @@ public class Capabilities {
 
         return driver;
     }
+
+
+        public static AndroidDriver<AndroidElement> androidCapabilitiesDevice () throws MalformedURLException {
+            File f = new File("src/test/resources");
+            File fs = new File(f,"original.apk");
+            URL servUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+            cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android device");
+            cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+            cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+            AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(servUrl, cap);
+
+            return driver;
+        }
+
+    public static AndroidDriver<AndroidElement> androidCapabilitiesChrome () throws MalformedURLException {
+
+        URL servUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android device");
+        cap.setCapability(MobileCapabilityType.BROWSER_NAME,"Chrome");
+        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(servUrl, cap);
+
+        return driver;
+    }
+
+
+    /*public static AndroidDriver<AndroidElement> androidCapabilitiesInstalledAPK () throws MalformedURLException {
+        //File f = new File("src/test/resources");
+        //File fs = new File(f,"original.apk");
+        URL servUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel2API28");
+        cap.setCapability(MobileCapabilityType.APP, "io.com.appium.android.apis");
+        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+        //cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(servUrl, cap);
+
+        return driver;
+    }*/
+
+    public static AndroidDriver<AndroidElement> iOSCapabilities () throws MalformedURLException {
+        File f = new File("src/test/resources");
+        File fs = new File(f,"original.ipa");
+        URL servUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.IOS);
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "SE");
+        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+        cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(servUrl, cap);
+
+        return driver;
+    }
+
 
 }
--- src/test/java/Gestures.java
+++ src/test/java/Gestures.java
@@ -1,14 +1,14 @@
 import io.appium.java_client.TouchAction;
-import static io.appium.java_client.touch.TapOptions.tapOptions;
-import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.TapOptions.tapOptions; //added
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions; //added
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 import io.appium.java_client.touch.TapOptions;
 import org.openqa.selenium.WebElement;
-import static java.time.Duration.ofSeconds;
-import static io.appium.java_client.touch.offset.ElementOption.element;
+import static java.time.Duration.ofSeconds; //added
+import static io.appium.java_client.touch.offset.ElementOption.element; //added
 
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
@@ -29,7 +29,7 @@ public class Gestures extends Capabilities {
 
         //Long Tap
         WebElement popUp = driver.findElementByXPath("//android.widget.TextView[@text='People Names']");
-        t.longPress(longPressOptions().withElement(element(popUp)).withDuration(ofSeconds(4))).release().perform();
+        t.longPress(longPressOptions().withElement(element(popUp)).withDuration(ofSeconds(2))).release().perform();
         //System.out.println(driver.findElementById("android:id/title").isDisplayed());
 
         driver.findElementByXPath("//*[@text='Sample action']").click();
@@ -43,6 +43,39 @@ public class Gestures extends Capabilities {
         driver.findElementByXPath("//*[@content-desc='9']").click();
 
         //Swipe
+        //Script:long press on element -> 2sec time out -> move to another element  -> release
+        WebElement swipeStart = driver.findElementByXPath("//*[@content-desc='15']");
+        WebElement swipeEnd = driver.findElementByXPath("//*[@content-desc='45']");
+        TouchAction swipe = new TouchAction(driver);
+        swipe.longPress(longPressOptions().withElement(element(swipeStart)).withDuration(ofSeconds(2))).moveTo(element(swipeEnd)).release().perform();
+
+        //Scrolling
+
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+        driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"));");
+
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+
+        //Drag n Dropp
+        //Scenario : Long press -> move to the destanation -> release
+        driver.findElementByXPath("//*[@text='Views']").click();
+        driver.findElementByXPath("//*[@text='Drag and Drop']").click();
+
+        TouchAction dragNdrop = new TouchAction(driver);
+        WebElement dragNdropStart = driver.findElementsByClassName("android.view.View").get(0);
+        WebElement dragNdropEnd = driver.findElementsByClassName("android.view.View").get(1);
+
+        dragNdrop.longPress(longPressOptions().withElement(element(dragNdropStart))).moveTo(element(dragNdropEnd)).release().perform();
+        //Shortcut way to do it if no need to use internal waits and others
+        // dragNdrop.longPress(element(dragNdropStart)).moveTo(element(dragNdropEnd)).release().perform();
+
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+
+
 
     }
 
