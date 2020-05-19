a6977724df2362d5dbee1304b9585f0cd8080b89
Andrey Sokolovskiy <andrey.sokolovskiy@ttms.pl>
2020-05-19 20:57:15 +0200

    E-comerce app test scenario added

---
+++ src/test/java/tttestNG/Main.java
@@ -0,0 +1,18 @@
+package tttestNG;
+
+import org.testng.annotations.Test;
+
+public class Main {
+
+    @Test
+    public void testCaseLogin()
+    {
+        System.out.println("Hello");
+    }
+    @Test
+    public void testCaseLogin2()
+    {
+        System.out.println("Hello");
+    }
+
+}
